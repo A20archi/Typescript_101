082e6126ae42def29b8c01e3000e8373824050b7
A20archi <b.saptarshi2004@gmail.com>
2025-09-19 01:04:22 +0530

    Demonstrating a typescript project

---
+++ 07TypescriptProj/src/index.ts
@@ -0,0 +1,14 @@
+
+console.log("Hello, TypeScript!");
+
+console.log("This is a TypeScript project setup example.");
+
+// You can add your TypeScript code here and it will be compiled to JavaScript in the dist folder.
+
+//just need to run `tsc -w` command in the terminal to compile the code
+
+console.log("Make sure to check the tsconfig.json file for configuration options.");
+
+// Also, check out the other files in this project for examples of TypeScript features like types, interfaces, and more.
+
+// To run the compiled JavaScript code, use the command `node dist/index.js` in the terminal.
